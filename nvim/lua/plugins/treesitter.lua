10f1cc39cdc018a66ef8b05a33a8056092536b1e
yutak03 <yutak03.dev@gmail.com>
2025-12-31 12:41:12 +0900

    Haskellを使えるように

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,32 +2,33 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   event = { "BufReadPre", "BufNewFile" },
-  main = "nvim-treesitter.configs",
-  opts = {
-    ensure_installed = {
-      "svelte",
-      "typescript",
-      "tsx",
-      "javascript",
-      "html",
-      "css",
-      "lua",
-      "python",
-      "rust",
-      "yaml",
-      "json",
-      "markdown",
-      "markdown_inline",
-      "haskell",
-    },
-    sync_install = false,
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    indent = {
-      enable = true,
-    },
-  },
+  config = function()
+    require("nvim-treesitter").setup({
+      ensure_installed = {
+        "svelte",
+        "typescript",
+        "tsx",
+        "javascript",
+        "html",
+        "css",
+        "lua",
+        "python",
+        "rust",
+        "yaml",
+        "json",
+        "markdown",
+        "markdown_inline",
+        "haskell",
+      },
+      sync_install = false,
+      auto_install = true,
+    })
+
+    -- Enable treesitter-based highlighting
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
+    })
+  end,
 }
